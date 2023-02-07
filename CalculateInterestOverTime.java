4f7bda255c9addfc49a55d0e27cfb2bb1787e599
PlasmaticJewel <123777068+PlasmaticJewel@users.noreply.github.com>
2023-02-07 13:39:51 -0500

    First commit
    This is my first commit

---
+++ CalculateInterestOverTime.java
@@ -0,0 +1,43 @@
+
+/*
+* Nathan Wheeler
+* 2/1/23
+* CSC-D01-2023sp-128649
+* Intro to the Java programming language
+* Chapter 2 HW
+* nrwheeler2201@student.stcc.edu
+* This program will take an amount of money after an investment, and take input of amount of time and the rate of interest, and will output the ammount of money invested.
+*/
+//Intro
+import java.util.Scanner;
+
+public class CalculateInterestOverTime {
+
+	public static void inputphrase(String input) {
+		System.out.println("Enter the " + input);
+	}
+
+	public static void main(String[] args) {
+
+//Gather inputs
+
+		System.out.println("Welcome to Interest over time calculator!");
+		inputphrase("final account value. \n -->");
+		Scanner scanner = new Scanner(System.in);
+		double output = scanner.nextDouble();
+		// System.out.println(output);
+		inputphrase("rate of interest in percent \n-->");
+		double interest = scanner.nextDouble();
+		inputphrase("time in years of the investment");
+		double time = scanner.nextDouble(); // in Years
+
+		mistake
+		
+		// Begin calculations
+		
+		double money;
+		money = output / Math.pow(1+interest, time);
+		System.out.println(money);
+		System.out.println("Thank you for using the investment calculator!");
+	}
+}
